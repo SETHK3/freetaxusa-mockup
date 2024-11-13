469410370742251350db99c326ad93006385ef67
sethk3 <threegartz@gmail.com>
2024-11-12 17:13:39 -0700

    adds navbar and its elements

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,4 +1,17 @@
-<template></template>
+<template>
+  <div class="home">
+    <nav class="navbar">
+      <img src="./logo.png" alt="FreeTaxUSA Logo" class="logo" />
+      <ul class="nav-links">
+        <li><a href="#">Pricing and Products</a></li>
+        <li><a href="#">Support</a></li>
+        <li><a href="#">Prior Year</a></li>
+        <li><a href="#">After You File</a></li>
+      </ul>
+      <button class="sign-in">Sign In</button>
+    </nav>
+  </div>
+</template>
 
 <script>
 export default {
